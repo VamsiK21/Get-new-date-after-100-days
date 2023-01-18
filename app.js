9556266ca193169545aa1731f3538b6b9de49434
VamsiK21 <121943954+VamsiK21@users.noreply.github.com>
2023-01-18 20:25:59 +0530

    updated app.js
    Adding days to today's date method placed inside the get method function

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const addDays = require("date-fns/addDays");
-const result = addDays(new Date(), 100);
-console.log(result);
-
 const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
+  const result = addDays(new Date(), 100);
   response.send(
     `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
   );
